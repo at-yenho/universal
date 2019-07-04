0f4abee44af6d5e4c597f3fb7a8b860836da81f0
at-yenho <yen.ho@asiantech.vn>
2019-07-04 11:22:32 +0700

    research universal and server-rendering

---
+++ src/app/app.server.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { ServerModule } from '@angular/platform-server';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {UniversalInterceptor} from './universal-interceptor';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    AppModule,
+    ServerModule,
+    ModuleMapLoaderModule
+  ],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: UniversalInterceptor,
+    multi: true
+  }],
+  bootstrap: [ AppComponent ],
+})
+export class AppServerModule {}
